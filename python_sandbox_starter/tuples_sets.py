c22eb3028d05d84cac4c7db79fd06a9badd339da
Toluwalase Adejuwon <toluwalasepeter100@gmail.com>
2024-01-31 22:28:32 +0100

    tuples Learnt

--- python_sandbox_starter/tuples_sets.py
+++ python_sandbox_starter/tuples_sets.py
@@ -1,5 +1,18 @@
 # A Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
- 
 
+# Creating tuples : Another way odf creating *list* but doesn't have a lot of functions like *list* and it's unchangeable
+fruits = ("Apple", "Pineapple", "Orange", "Grape")
+print(fruits)
 
-# A Set is a collection which is unordered and unindexed. No duplicate members.
+# Always remember to use comma after a single value  
+fruits3 = ("Boats")
+print(fruits3, type(fruits3))
+
+fruits2 = ("Boats",)
+print(fruits2, type(fruits2))
+
+# A Set is a collection which is unordered and un-indexed. No duplicate members.
+fruit_set = {'Apples', 'Orange', 'Mangoes'}
+
+print(fruit_set)
+
