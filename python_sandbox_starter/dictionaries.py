4f87e21e1b2315d1b43a49c2a99e139c12d27a6b
Toluwalase Adejuwon <toluwalasepeter100@gmail.com>
2024-02-01 14:29:14 +0100

    Completed learning dictionaries from the crash course

--- python_sandbox_starter/dictionaries.py
+++ python_sandbox_starter/dictionaries.py
@@ -17,6 +17,39 @@ university_colleges = {
 
 first_college = university_colleges["colphys"]
 print(first_college)
-
 print("I am going with", university_colleges['Senate_building'], "to the ceremonial building at ", university_colleges["colaminin"])
+
+
+university_colleges['phone_number'] = '2349038358375'
+print(university_colleges)
+
+key_value = university_colleges.keys()
+print(key_value)
+
+item_value = university_colleges.items()
+print(item_value)
+
+
+copy_value = university_colleges.copy()
+print(copy_value)
+copy_value["add_value"] = "my_name_is_Toluwalase"
+print(copy_value)
+del(copy_value['Senate_building'])
+print(copy_value)
+
+
+# dictionaries length
+print(len(university_colleges))
+print(len(copy_value))
+
+# Dictionary in List : Is the same thing with
+    # Object in Array
+dict_list = [
+    {'name': 'Toluwalase', 'age': '12', 'color': "Blue"},
+    {'name': 'Peter', 'age': '45', 'color': "Pink"},
+    {'name': 'Sophia', 'age': '909', 'color': "White"},
+]
+
+first_dict_name = dict_list[0]['name']
+print(first_dict_name)
 
